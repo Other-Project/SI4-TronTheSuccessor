93269367207df277b3f079291d57eef0b22a11af
Alexandre-Gripari <alexandre.gripari@etu.univ-cotedazur.fr>
2025-01-15 20:16:49 +0100

    fix(#4): draw demo hexagon correctly

--- services/files/front/components/control/control.js
+++ services/files/front/components/control/control.js
@@ -1,39 +1,69 @@
 export class Control extends HTMLElement {
+    gridSize = [2, 3];
+    cellSize = 100;
+    playerSize = 75;
+    playerColors = ["#242424", "#D732A8", "#32BED7"];
     constructor() {
         super();
 
         const shadow = this.attachShadow({mode: "open"});
+
+        let stylesheet = document.createElement("link");
+        stylesheet.href = "/components/game-board/game-board.css";
+        stylesheet.rel = "stylesheet";
+        stylesheet.type = "text/css";
+        shadow.appendChild(stylesheet);
+
         this.canvas = document.createElement("canvas");
-        this.canvas.style.maxWidth = "100%";
-        this.canvas.style.maxHeight = "100%";
-        this.canvas.width = 300;
-        this.canvas.height = 250;
         this.ctx = this.canvas.getContext("2d");
         shadow.appendChild(this.canvas);
 
         this.#draw();
     }
 
+    #cellCoordinates(x, y) {
+        return [x * this.cellSize + (y * this.cellSize / 2) % this.cellSize, y * this.cellSize * 3 / 4];
+    }
 
     #draw() {
-        this.#drawHelp(2, 3, 100);
+        this.canvas.width = this.gridSize[0] * this.cellSize + this.cellSize / 2;
+        this.canvas.height = this.cellSize + (this.gridSize[1] - 1) * this.cellSize * 3 / 4;
+        this.#drawHexagons();
+        this.#drawPlayer(1, 8, 1);
+        this.#drawPlayer(14, 0, 2);
     }
 
-    #drawHelp(xNb, yNb, size) {
-        for (let y = 0; y < yNb; y++)
-            for (let x = 0; x < xNb + y % 2; x++)
-                this.#drawHexagon(x * size + (y * size / 2) % size + size / 4, y * size * 3 / 4 + size / 4, size);
+    #drawHexagons() {
+        for (let y = 0; y < this.gridSize[1]; y++)
+            for (let x = 0; x < this.gridSize[0] + x % 2; x++)
+                this.#drawHexagon(x, y, x < 2 ? 1 : x > 13 ? 2 : null);
     }
 
-    #drawHexagon(x, y, size) {
+    #drawHexagon(x, y, owner = null) {
+        [x, y] = this.#cellCoordinates(x, y);
         this.ctx.beginPath();
-        this.ctx.moveTo(x + size / 2, y);
-        this.ctx.lineTo(x, y + size / 4);
-        this.ctx.lineTo(x, y + size * 3 / 4);
-        this.ctx.lineTo(x + size / 2, y + size);
-        this.ctx.lineTo(x + size, y + size * 3 / 4);
-        this.ctx.lineTo(x + size, y + size / 4);
-        this.ctx.lineTo(x + size / 2, y);
+        this.ctx.moveTo(x + this.cellSize / 2, y);
+        this.ctx.lineTo(x, y + this.cellSize / 4);
+        this.ctx.lineTo(x, y + this.cellSize * 3 / 4);
+        this.ctx.lineTo(x + this.cellSize / 2, y + this.cellSize);
+        this.ctx.lineTo(x + this.cellSize, y + this.cellSize * 3 / 4);
+        this.ctx.lineTo(x + this.cellSize, y + this.cellSize / 4);
+        this.ctx.lineTo(x + this.cellSize / 2, y);
+        this.ctx.strokeStyle = "#656565";
+        this.ctx.lineWidth = 3;
         this.ctx.stroke();
+        this.ctx.fillStyle = this.playerColors[owner] || this.playerColors[0];
+        this.ctx.fill();
+    }
+
+    #drawPlayer(x, y, player) {
+        [x, y] = this.#cellCoordinates(x, y);
+        let playerImg = new Image();
+        playerImg.onload = () => this.ctx.drawImage(playerImg,
+            x + (this.cellSize - this.playerSize) / 2,
+            y + (this.cellSize - this.playerSize) / 2,
+            this.playerSize,
+            this.playerSize);
+        playerImg.src = `assets/player_${player}.png`;
     }
 }
--- services/files/front/js/modules.js
+++ services/files/front/js/modules.js
@@ -1,9 +1,7 @@
-import { GameBoard } from "../components/game-board/game-board.js";
-import {GameBoard} from "./game-board.js";
+import {GameBoard} from "../components/game-board/game-board.js";
 import {HelpPage} from "../components/help-page/help-page.js"
 import {Control} from "../components/control/control.js"
 
 customElements.define("app-game-board", GameBoard);
 customElements.define("app-help-page", HelpPage);
 customElements.define("app-control", Control);
-

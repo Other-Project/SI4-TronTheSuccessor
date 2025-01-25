7910b3ebb3be1347a2f49139a4a3123dc7bae25a
Alexandre-Gripari <alexandre.gripari@etu.univ-cotedazur.fr>
2025-01-25 21:18:35 +0100

    feat(#14): simple monte carlo

--- services/files/front/js/tron-the-successor.js
+++ services/files/front/js/tron-the-successor.js
@@ -1,4 +1,6 @@
 const moves = ["KEEP_GOING", "LIGHT_RIGHT", "HEAVY_RIGHT", "NONE", "HEAVY_LEFT", "LIGHT_LEFT"];
+const number_of_games = 5000;
+const memo = new Map();
 let direction = 0;
 let gameBoard = Array.from(Array(9), (_, i) => Array(i % 2 === 0 ? 16 : 15).fill(0));
 let allAdjacent = [];
@@ -15,7 +17,7 @@ async function setup(playersState) {
 async function nextMove(playersState) {
     gameBoard[playersState.playerPosition.row - 1][playersState.playerPosition.column - 1] = 1;
     gameBoard[playersState.opponentPosition.row - 1][playersState.opponentPosition.column - 1] = 1;
-    const coord = determineNextMove(playersState);
+    const coord = determineNextBestMove(playersState);
     const move = (coord - direction + 6) % 6;
     console.log("current_direction: ", direction, "next_hex: ", coord, "move_to_hex: ", moves[move]);
     if (moves[move] === "NONE") {
@@ -25,6 +27,58 @@ async function nextMove(playersState) {
     }
     direction = coord;
     return moves[move];
+}
+
+function determineNextBestMove(playersState) {
+    const possibleMoves = getPossibleMoves(playersState.playerPosition);
+    const winRates = possibleMoves.map(move => simulateGames(playersState, move));
+    const bestMoveIndex = winRates.indexOf(Math.max(...winRates));
+    console.log("win_rates: ", winRates);
+    return allAdjacent[playersState.playerPosition.row - 1][playersState.playerPosition.column - 1]
+        .indexOf(possibleMoves[bestMoveIndex]);
+}
+
+function simulateGames(playersState, move) {
+    const key = JSON.stringify({playersState, move});
+    if (memo.has(key)) return memo.get(key);
+
+    let wins = 0;
+    for (let i = 0; i < number_of_games; i++) {
+        if (simulateGame(playersState, move)) wins++;
+    }
+    const winRate = wins / number_of_games;
+    memo.set(key, winRate);
+    return winRate;
+}
+
+function simulateGame(playersState, move) {
+    let tempBoard = structuredClone(gameBoard);
+    let tempPosition = [playersState.playerPosition.row - 1, playersState.playerPosition.column - 1];
+    let opponentPosition = [playersState.opponentPosition.row - 1, playersState.opponentPosition.column - 1];
+    let tempDirection = move;
+    let opponentDirection = getPossibleMoves(playersState.opponentPosition)[0];
+
+    for (let i = 0; i < 144; i++) {
+        tempPosition = tempDirection;
+        if (!tempBoard[tempPosition[1]] || tempBoard[tempPosition[1]][tempPosition[0]] !== 0) return false;
+        tempBoard[tempPosition[1]][tempPosition[0]] = 1;
+
+        opponentPosition = opponentDirection;
+        if (!tempBoard[opponentPosition[1]] || tempBoard[opponentPosition[1]][opponentPosition[0]] !== 0) return true;
+        tempBoard[opponentPosition[1]][opponentPosition[0]] = 2;
+        const possibleMoves = getPossibleMoves({
+            row: tempPosition[1] + 1,
+            column: tempPosition[0] + 1
+        });
+        tempDirection = possibleMoves[Math.floor(Math.random() * possibleMoves.length)];
+
+        const opponentMoves = getPossibleMoves({
+            row: opponentPosition[1] + 1,
+            column: opponentPosition[0] + 1
+        });
+        opponentDirection = opponentMoves[Math.floor(Math.random() * opponentMoves.length)];
+    }
+    return false;
 }
 
 export {setup, nextMove}; // ES6

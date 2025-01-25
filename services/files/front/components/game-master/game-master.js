757570c31cf52ef5c59b8f6c7fa7a758cd08d557
Evan Galli <evan_g@orange.fr>
2025-01-25 17:55:22 +0100

    feat(#15): Popup at the end of the game [WIP]

--- services/files/front/components/game-master/game-master.js
+++ services/files/front/components/game-master/game-master.js
@@ -6,23 +6,36 @@ export class GameMaster extends HTMLComponent {
     gridSize = [16, 9];
 
     constructor() {
-        super();
-        this.gameBoard = document.createElement("app-game-board");
-        this.shadowRoot.appendChild(this.gameBoard);
+        super("game-master", ["html", "css"]);
     }
 
-    onVisible = () => {
+    onSetupCompleted = () => {
+        this.gameBoard = this.shadowRoot.getElementById("board");
+        this.popupWindow = this.shadowRoot.getElementById("popup-container");
+        this.popupWindow.style.display = "none";
+    }
+
+    onVisible = () => this.newGame();
+    onHidden = () => this.stopGame();
+
+    newGame() {
+        this.stopGame();
         this.game = new Game(this.gridSize[0], this.gridSize[1], new HumanPlayer("Player 1", 1), new HumanPlayer("Player 2", 2), 500);
         this.game.addEventListener("game-turn", (e) => {
-            if (e.detail.ended) alert(e.detail.draw ? "Draw" : e.detail.winner.name + " won");
+            if (e.detail.ended) this.endScreen(e.detail);
             this.gameBoard.draw(this.game);
         });
         this.game.start();
         this.gameBoard.draw(this.game);
     }
 
-    onHidden = () => {
+    stopGame() {
         if (this.game) this.game.stop();
         this.game = undefined;
+    }
+
+    endScreen(details) {
+        this.popupWindow.style.display = "block";
+        alert(details.draw ? "Draw" : details.winner.name + " won");
     }
 }

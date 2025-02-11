62117d3b0e8500bdb01fa590f1fc983725996ddd
Evan Galli <evan.galli@etu.univ-cotedazur.fr>
2025-02-11 17:53:48 +0100

    feat(#25): Navigation history

--- services/files/front/components/pages/pages.js
+++ services/files/front/components/pages/pages.js
@@ -7,10 +7,19 @@ export class Pages extends HTMLComponent {
             if (event.detail && typeof event.detail === "object") this.#showElement(event.detail.name, event.detail.attr);
             else this.#showElement(event.detail);
         });
+
+        window.addEventListener("hashchange", () => {
+            this.#showElement(this.#readHash() ?? "home");
+        });
     }
 
     onVisible = () => {
-        this.#showElement("home");
+        this.#showElement(this.#readHash() ?? "home");
+    };
+
+    #readHash() {
+        const hash = location.hash.substring(1);
+        return hash === "" ? null : hash;
     }
 
     /**
@@ -19,11 +28,17 @@ export class Pages extends HTMLComponent {
      */
     #showElement(elementId, attr = undefined) {
         const elements = this.shadowRoot.querySelectorAll("#pages > *");
+        const idExists = Array.from(elements.values()).some(el => el.id);
+        if (!idExists) return;
+
         elements.forEach(element => {
             if (element.id === elementId) {
                 element.style.display = "block";
                 if (attr) Object.entries(attr).forEach(([k, v]) => element.setAttribute(k, v));
             } else element.style.display = "none";
         });
+
+        if (history.state !== elementId)
+            history.pushState(elementId, "", `#${elementId}`);
     }
 }

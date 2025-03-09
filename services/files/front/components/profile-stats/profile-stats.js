ae181bcc4b15be56a392d63933a623337ac4eba9
Alexandre-Gripari <alexandre.gripari@etu.univ-cotedazur.fr>
2025-03-09 19:01:43 +0100

    feat(#32): base template for profile overview

--- services/files/front/components/profile-stats/profile-stats.js
+++ services/files/front/components/profile-stats/profile-stats.js
@@ -1,7 +1,38 @@
 import {HTMLComponent} from "/js/component.js";
 
 export class ProfileStats extends HTMLComponent {
+    games;
+    winrate;
+    time;
+    streak;
+
     constructor() {
         super("profile-stats", ["html", "css"]);
+    }
+
+    onSetupCompleted = () => {
+        const template = document.getElementById("stats-template").content.cloneNode(true);
+        this.shadowRoot.appendChild(template);
+
+        this.games = this.shadowRoot.getElementById("games");
+        this.winrate = this.shadowRoot.getElementById("winrate");
+        this.time = this.shadowRoot.getElementById("time");
+        this.streak = this.shadowRoot.getElementById("streak");
+        this.#updateStats();
+    };
+
+    static get observedAttributes() {
+        return ["games", "winrate", "time", "streak"];
+    }
+
+    attributeChangedCallback() {
+        this.#updateStats();
+    }
+
+    #updateStats() {
+        this.elements.games.textContent = this.getAttribute("games") || "0";
+        this.elements.winrate.textContent = `${this.getAttribute("winrate") || "0"}%`;
+        this.elements.time.textContent = this.getAttribute("time") || "0h";
+        this.elements.streak.textContent = this.getAttribute("streak") || "0";
     }
 }

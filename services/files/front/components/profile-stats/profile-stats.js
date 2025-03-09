22023aaf5173d2bb8e8c3fb7ce9367b90000ee03
Alexandre-Gripari <alexandre.gripari@etu.univ-cotedazur.fr>
2025-03-09 21:13:17 +0100

    feat(#32): mock-up for overview

--- services/files/front/components/profile-stats/profile-stats.js
+++ services/files/front/components/profile-stats/profile-stats.js
@@ -1,19 +1,11 @@
 import {HTMLComponent} from "/js/component.js";
 
 export class ProfileStats extends HTMLComponent {
-    games;
-    winrate;
-    time;
-    streak;
-
     constructor() {
         super("profile-stats", ["html", "css"]);
     }
 
     onSetupCompleted = () => {
-        const template = document.getElementById("stats-template").content.cloneNode(true);
-        this.shadowRoot.appendChild(template);
-
         this.games = this.shadowRoot.getElementById("games");
         this.winrate = this.shadowRoot.getElementById("winrate");
         this.time = this.shadowRoot.getElementById("time");
@@ -30,9 +22,11 @@ export class ProfileStats extends HTMLComponent {
     }
 
     #updateStats() {
-        this.elements.games.textContent = this.getAttribute("games") || "0";
-        this.elements.winrate.textContent = `${this.getAttribute("winrate") || "0"}%`;
-        this.elements.time.textContent = this.getAttribute("time") || "0h";
-        this.elements.streak.textContent = this.getAttribute("streak") || "0";
+        if (!this.games || !this.winrate || !this.time || !this.streak) return;
+
+        this.games.textContent = this.getAttribute("games") || "10";
+        this.winrate.textContent = `${this.getAttribute("winrate") || "50"}%`;
+        this.time.textContent = this.getAttribute("time") || "5h";
+        this.streak.textContent = this.getAttribute("streak") || "3";
     }
 }

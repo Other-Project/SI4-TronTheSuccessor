66e3656f15591a4fc369b9b04c3c79125a31e674
Evan Galli <evan_g@orange.fr>
2025-04-22 22:57:44 +0200

    fix(#81): Buttons and inputs bug on iOS Webkit

--- services/files/front/components/button/button.js
+++ services/files/front/components/button/button.js
@@ -1,6 +1,8 @@
 import {HTMLComponent} from "/js/component.js";
 
 export class Button extends HTMLComponent {
+    background;
+    pulse;
 
     static get observedAttributes() {
         return ["background", "pulse"];
@@ -8,21 +10,24 @@ export class Button extends HTMLComponent {
 
     constructor() {
         super("button", ["css"]);
-        this.button = document.createElement("button");
-        this.button.appendChild(document.createElement("slot"));
-        this.shadowRoot.appendChild(this.button);
+        const btn = document.createElement("button");
+        btn.appendChild(document.createElement("slot"));
+        this.shadowRoot.appendChild(btn);
     }
 
     attributeChangedCallback(name, oldValue, newValue) {
         super.attributeChangedCallback(name, oldValue, newValue);
-        if (name === "background" && oldValue)
-                this.button.classList.remove(oldValue);
+        if (name === "background" && oldValue) this.button.classList.remove(oldValue);
         this.#refresh();
     }
 
-    onSetupCompleted = () => this.#refresh();
+    onSetupCompleted = () => {
+        this.button = this.shadowRoot.querySelector("button");
+        this.#refresh();
+    }
 
     #refresh() {
+        if (!this.button) return;
         this.button.classList.add(this.background);
         this.button.classList.toggle("pulse", this.pulse === "true");
     }

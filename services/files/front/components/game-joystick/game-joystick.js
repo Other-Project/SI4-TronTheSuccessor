98d0e7d12d59dcf3509311997463016b0452f227
Alexandre-Gripari <alexandre.gripari@etu.univ-cotedazur.fr>
2025-04-23 18:54:39 +0200

    fix(#77): new joystick using nipplejs

--- services/files/front/components/game-joystick/game-joystick.js
+++ services/files/front/components/game-joystick/game-joystick.js
@@ -1,61 +1,39 @@
 import {HTMLComponent} from "/js/component.js";
+import nipplejs from "https://cdn.jsdelivr.net/npm/nipplejs@0.10.2/+esm";
+import {directionToAngle} from "/js/player.js";
 
 export class GameJoystick extends HTMLComponent {
+    direction = "none";
+
     constructor() {
-        super("game-joystick", ["html", "css"]);
+        super("game-joystick", ["css"]);
     }
 
     onSetupCompleted = () => {
-        this.container = this.shadowRoot.getElementById("joystick-container");
-        this.base = this.shadowRoot.getElementById("joystick-base");
-        this.knob = this.shadowRoot.getElementById("joystick-knob");
-        this.center = {x: 0, y: 0};
-        this.direction = null;
-        this.base.addEventListener("touchstart", this.onTouchStart.bind(this));
-        this.base.addEventListener("touchmove", this.onTouchMove.bind(this));
-        this.base.addEventListener("touchend", this.onTouchEnd.bind(this));
+        const joystickContainer = document.createElement("div");
+        const joystickZone = document.createElement("div");
+        joystickContainer.id = "joystick-zone";
+        joystickContainer.appendChild(joystickZone);
+        this.shadowRoot.appendChild(joystickContainer);
+
+        const size = 150;
+        const joystick = nipplejs.create({
+            zone: joystickZone,
+            mode: "static",
+            position: {left: "50%", top: "50%"},
+            color: "#000000",
+            size: size
+        });
+
+        joystick.on("move", (evt, data) => {
+            let direction;
+            if (data.force < 0.2) return;
+            const angle = Math.round(data.angle.degree / 45) * 45;
+            direction = Object.entries(directionToAngle).find(([_, value]) => value === angle);
+            if (direction) {
+                this.direction = direction[0];
+                this.dispatchEvent(new CustomEvent("joystick-direction", {detail: this.direction}));
+            }
+        });
     };
-
-    onTouchStart = (e) => {
-        e.preventDefault();
-        const rect = this.base.getBoundingClientRect();
-        this.center = {x: rect.left + rect.width / 2, y: rect.top + rect.height / 2};
-    }
-
-    onTouchMove = (e) => {
-        e.preventDefault();
-
-        const touch = e.touches[0];
-        const dx = touch.clientX - this.center.x;
-        const dy = touch.clientY - this.center.y;
-        const distance = Math.min(Math.sqrt(dx * dx + dy * dy), 60);
-        const angle = Math.atan2(dy, dx);
-
-        this.knob.style.left = `calc(40% + ${Math.cos(angle) * distance}px)`;
-        this.knob.style.top = `calc(40% + ${Math.sin(angle) * distance}px)`;
-
-        let direction = null;
-        if (Math.abs(dx) < 15 && Math.abs(dy) < 15) return;
-
-        if (dy < -30 && dx < -30) direction = "up-left";
-        else if (dy < -30 && dx > 30) direction = "up-right";
-        else if (dy > 30 && dx < -30) direction = "down-left";
-        else if (dy > 30 && dx > 30) direction = "down-right";
-        else if (dx < -30) direction = "left";
-        else if (dx > 30) direction = "right";
-
-        if (direction && direction !== this.direction) {
-            this.direction = direction;
-            this.dispatchEvent(new CustomEvent("joystick-direction", {
-                detail: {direction},
-                bubbles: true,
-                composed: true
-            }));
-        }
-    }
-
-    onTouchEnd = () => {
-        this.knob.style.left = `50%`;
-        this.knob.style.top = `50%`;
-    }
 }
--- services/files/front/js/human-player.js
+++ services/files/front/js/human-player.js
@@ -56,19 +56,16 @@ export class HumanPlayer extends Player {
         this.#keypressed.add(e.key.toUpperCase());
         let direction = Object.entries(this.keys)
             .find(([_, keyComp]) => keyComp.every(k => Array.from(this.#keypressed).some(value => value.includes(k.toUpperCase()))));
-        if (direction) {
-            super.setNextDirection(direction[0]);
-            this.dispatchEvent(new CustomEvent("player-direction", {
-                detail: {
-                    direction: direction[0],
-                    number: this.number
-                }
-            }));
-        }
+        if (direction) this.#changeDirection(direction[0]);
     }
 
     #onJoystickMove = e => {
         const direction = e.detail.direction;
+        console.log(direction);
+        this.#changeDirection(direction);
+    };
+
+    #changeDirection(direction) {
         super.setNextDirection(direction);
         this.dispatchEvent(new CustomEvent("player-direction", {
             detail: {
@@ -76,7 +73,7 @@ export class HumanPlayer extends Player {
                 number: this.number
             }
         }));
-    };
+    }
 
     #onKeyReleased = e => this.#keypressed.delete(e.key.toUpperCase());
 }

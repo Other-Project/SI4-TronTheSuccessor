722bfa15c494f909b6dbfc09ec44098ffe3aa728
Evan Galli <evan.galli@etu.univ-cotedazur.fr>
2025-04-23 17:17:04 +0200

    refactor(#84): Rework the action buttons in the profile page

--- services/files/front/components/profile-page/profile-page.js
+++ services/files/front/components/profile-page/profile-page.js
@@ -1,6 +1,8 @@
 import {HTMLComponent} from "/js/component.js";
 import {fetchApi, getUserInfo} from "/js/login-manager.js";
 import {changePage} from "/components/pages/pages.js";
+import notificationService from "../../js/notification.js";
+import "/js/capacitor.min.js";
 
 export class ProfilePage extends HTMLComponent {
     constructor() {
@@ -18,9 +20,41 @@ export class ProfilePage extends HTMLComponent {
         this.tabNavigation = this.shadowRoot.getElementById("tab-navigation");
 
         this.shadowRoot.getElementById("home-page").addEventListener("click", () => changePage("/"));
+
+        this.share = this.shadowRoot.getElementById("share");
+        this.share.addEventListener("click", () => {
+            if (Capacitor.isNativePlatform())
+                Capacitor.Plugins.Share.share({
+                    title: "Tron: The Successor",
+                    text: "I want to share this Tron: The Successor profile with you",
+                    url: new URL("https://tronsuccessor.ps8.pns.academy", location.pathname).toString(),
+                    dialogTitle: "Share this profile"
+                });
+            else navigator.clipboard.writeText(location.href)
+                .then(() => this.#showNotification("Profile URL copied to clipboard!", 2000, "#8E24AA", "white"));
+        });
+
+        this.addFriend = this.shadowRoot.getElementById("add-friend");
+        this.removeFriend = this.shadowRoot.getElementById("remove-friend");
+        this.addFriend.addEventListener("click", async () => await this.#manageFriend(this.stats.username, "add"));
+        this.removeFriend.addEventListener("click", async () => await this.#manageFriend(this.stats.username, "remove"));
+        notificationService.addEventListener("refresh-friend-list", async () => await this.#refresh());
     };
 
-    onVisible = async () => {
+    onVisible = () => this.#refresh().then();
+
+    async #getStatus(username) {
+        if (this.stats.username === this.stats.loggedusername) return {isYou: true, isFriend: false, isPending: false};
+        const response = await fetchApi("/api/user/friends", {method: "GET"});
+        const friends = response.ok ? await response.json() : null;
+        return {
+            isYou: false,
+            isFriend: friends?.friends?.includes(username) ?? false,
+            isPending: friends?.pending?.includes(username) ?? false
+        };
+    };
+
+    async #refresh() {
         const urlPath = window.location.pathname.split("/");
         if (urlPath[1] !== "profile") return;
         let userName = urlPath[2];
@@ -35,20 +69,53 @@ export class ProfilePage extends HTMLComponent {
             return;
         }
 
+        this.share.classList.toggle("hidden", true);
+        this.addFriend.classList.toggle("hidden", true);
+        this.removeFriend.classList.toggle("hidden", true);
+
         this.usernameElement.textContent = userName;
         const response = await fetchApi(`/api/game/stats/${userName}`, undefined, false);
         this.profilePage.classList.toggle("not-found", !response.ok);
         this.notFoundUser.textContent = userName;
         if (!response.ok) return;
 
-        const stats = await response.json();
-        stats.username = userName;
-        stats.loggedusername = loggedUser;
-        if (stats) {
-            this.overview.setAttribute("stats", JSON.stringify(stats));
-            this.leaderboard.setAttribute("stats", JSON.stringify(stats));
-        }
+        this.stats = await response.json();
+        this.stats.username = userName;
+        this.stats.loggedusername = loggedUser;
+        this.overview.setAttribute("stats", JSON.stringify(this.stats));
+        this.leaderboard.setAttribute("stats", JSON.stringify(this.stats));
         this.history.dataset.tabDisabled = userName === loggedUser ? "false" : "true";
         this.tabNavigation.changeTab("overview");
-    };
+        this.share.classList.toggle("hidden", false);
+
+        const {isYou, isFriend, isPending} = await this.#getStatus(this.stats.username);
+        this.addFriend.classList.toggle("hidden", isYou || isFriend);
+        this.removeFriend.classList.toggle("hidden", isYou || isPending || !isFriend);
+        this.addFriend.button.disabled = isPending;
+        this.addFriend.title = isPending ? "Friend request already sent" : "";
+    }
+
+    async #manageFriend(friend, add) {
+        const response = await fetchApi(`/api/user/friends/${friend}`, {
+            method: add ? "POST" : "DELETE"
+        });
+        if (response.ok) {
+            if (add) this.#showNotification("Friend request sent!", 2000, "#8E24AA", "white");
+            else this.#showNotification("Friend removed", 2000, "#8E24AA", "white");
+        } else {
+            const error = await response.json();
+            this.#showNotification(`Error: ${error.error}`, 2000, "red", "white");
+        }
+    }
+
+    #showNotification(message, duration, background, color) {
+        document.dispatchEvent(new CustomEvent("show-notification", {
+            detail: {
+                message: message,
+                duration: duration,
+                background: background,
+                color: color
+            }
+        }));
+    }
 }
